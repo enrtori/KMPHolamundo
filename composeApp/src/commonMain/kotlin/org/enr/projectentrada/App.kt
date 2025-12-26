f54320044849a6f91e0bff466163dc1834c94514
enrtori <enriquetoricesaparicio@gmail.com>
2025-12-26 14:19:45 +0100

    add de estructura y condicion

--- composeApp/src/commonMain/kotlin/org/enr/projectentrada/App.kt
+++ composeApp/src/commonMain/kotlin/org/enr/projectentrada/App.kt
@@ -4,8 +4,10 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +16,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -25,13 +28,23 @@ import helloworld.composeapp.generated.resources.compose_multiplatform
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        //Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        var name: String by remember { mutableStateOf("") }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-             TextField(
+       var name: String by remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(3.dp))
+            TextField(
                 value = name,
-                onValueChange ={name =it}
+                onValueChange = { name = it }
             )
+            Spacer(modifier = Modifier.height(3.dp))
+            if (name.isNotEmpty()) {
+                Text("Hola " + name)
+            }
         }
 
         /*Column(
